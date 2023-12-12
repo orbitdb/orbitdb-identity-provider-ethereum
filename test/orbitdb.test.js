3ecc9d79303d56f38d61633aa37ea84316e8c30c
Hayden Young <haydenyoung@wijiti.com>
2023-12-12 06:43:02 +0000

    chore: Linting.

--- test/orbitdb.test.js
+++ test/orbitdb.test.js
@@ -1,7 +1,6 @@
 import assert from 'assert'
-import path from 'path'
 import { rimraf } from 'rimraf'
-import { createOrbitDB, KeyStore, Identities, useIdentityProvider } from '@orbitdb/core'
+import { createOrbitDB, Identities } from '@orbitdb/core'
 import OrbitDBIdentityProviderEthereum from '../src/index.js'
 import createWallet from './utils/create-wallet.js'
 import * as IPFS from 'ipfs-core'
@@ -16,23 +15,23 @@ describe('Use Ethereum Identity Provider with OrbitDB', function () {
     wallet = await createWallet()
     provider = OrbitDBIdentityProviderEthereum({ wallet })
   })
-  
+
   afterEach(async () => {
     await ipfs.stop()
     await rimraf('./orbitdb')
   })
-  
+
   it('should be passed using identity.provider', async () => {
     const orbitdb = await createOrbitDB({ ipfs, identity: { provider } })
-    
+
     assert.strictEqual(orbitdb.identity.id, wallet.address)
   })
-  
+
   it('should be passed as an preconfigured identity', async () => {
     const identities = await Identities({ ipfs })
     const identity = await identities.createIdentity({ id: 'userA', provider })
     const orbitdb = await createOrbitDB({ ipfs, identities, identity })
-    
+
     assert.strictEqual(orbitdb.identity.id, wallet.address)
-  })  
+  })
 })
